2798625322aa60e6d3db94609000f563475da343
MOHAMEDAfrath <afrathismath@gmail.com>
2021-07-14 21:12:25 +0530

    [Afrath] Welcome to data strucutre program

---
+++ DataStructureProgram/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DataStructureProgram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to data structure program");
+        }
+    }
+}
