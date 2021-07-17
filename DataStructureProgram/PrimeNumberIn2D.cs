70cb665754ad7e447f46b2bc1f6a1ee480fa929c
MOHAMEDAfrath <afrathismath@gmail.com>
2021-07-17 18:22:26 +0530

    [Afrath] Updated the file with Prime Number and anagram reverse using stack

--- DataStructureProgram/PrimeNumberIn2D.cs
+++ DataStructureProgram/PrimeNumberIn2D.cs
@@ -6,8 +6,10 @@ using System.Threading.Tasks;
 
 namespace DataStructureProgram
 {
+
     public class PrimeNumberIn2D<T> where T:IComparable
     {
+       
         public  int range = 0, index = 0, change = 0;
         public void PrimeChecker(int start, int end)
         {
@@ -82,8 +84,10 @@ namespace DataStructureProgram
                         if (SecondString == firstString)
                         {
                             Anagram[range, index] = primeNumbers[k, l];
+                            Push(Anagram[range,index]);
                             index++;
                             Anagram[range, index] = primeNumbers[k, q];
+                            Push(Anagram[range,index]);
                             index++;
 
                         }
@@ -91,6 +95,8 @@ namespace DataStructureProgram
                 }
                 Console.WriteLine(" ");
             }
+            Console.WriteLine(" ");
+            
             Console.WriteLine("Anagram Numbers in Range ({0} - {1})", start, end);
             for (int k = 0; k < 10; k++)
             {
@@ -104,6 +110,9 @@ namespace DataStructureProgram
                 }
                Console.WriteLine(" ");
             }
+            Console.WriteLine("Anagram in Reverse using Stack");
+            Console.WriteLine(" ");
+            Display();
             Console.WriteLine(" Not Anagram Numbers in Range {0} - {1}", start, end);
             int check = 0; ;
             for (int k = 0; k < 10; k++)
@@ -140,6 +149,31 @@ namespace DataStructureProgram
                 Console.WriteLine(" ");
             }
 
+        }
+        Node<T> top = null;
+        public void Push(T data)
+        {
+            Node<T> newnode = new Node<T>(data);
+            if (top == null)
+            {
+                newnode.next = null;
+            }
+            else
+            {
+                newnode.next = this.top;
+            }
+            this.top = newnode;
+
+        }
+        public void Display()
+        {
+            Node<T> temp = this.top;
+            while (temp != null)
+            {
+                Console.Write(temp.data + " ");
+                temp = temp.next;
+            }
+            Console.WriteLine(" ");
         }
     }
 }
